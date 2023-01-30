95c3d463de3e0af6d29b51ca7d2ae7b6d0dfdf2b
ungoodman <suchanej.19@gmail.com>
2023-01-30 23:31:08 +0700

    Add solenoid

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,17 @@
 char ssid[] = "3bb-wlan_2.4GHz";
 char pass[] = "1111100000";
 
+bool flagDryLimit = false;
+
+#define ACTIVE_LOW
+#ifdef ACTIVE_LOW
+bool solenoidStatus1 = HIGH;
+bool solenoidStatus2 = HIGH;
+#else
+bool solenoidStatus1 = LOW;
+bool solenoidStatus2 = LOW;
+#endif
+
 BlynkTimer timer;
 
 // This function is called every time the Virtual Pin 0 state changes
@@ -42,6 +53,34 @@ BLYNK_CONNECTED()
   Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
 }
 
+void checkSoilMoisture(int rawMoistureValue)
+{
+  if (rawMoistureValue > 700 && !flagDryLimit)
+  {
+    solenoidStatus1 = LOW;
+    solenoidStatus2 = LOW;
+
+    digitalWrite(16, solenoidStatus1);
+    digitalWrite(5, solenoidStatus2);
+    
+    flagDryLimit = true;
+    return;
+  }
+
+  if (rawMoistureValue < 400 && flagDryLimit)
+  {
+    solenoidStatus1 = HIGH;
+    solenoidStatus2 = HIGH;
+    
+    digitalWrite(16, solenoidStatus1);
+    digitalWrite(5, solenoidStatus2);
+    
+    flagDryLimit = false;
+    return;
+  }
+}
+
+
 // This function sends Arduino's uptime every second to Virtual Pin 2.
 void myTimerEvent()
 {
@@ -49,8 +88,10 @@ void myTimerEvent()
   // Please don't send more that 10 values per second.
   int analog = analogRead(A0);
   float percent = (102 - (analog / 10));
+  checkSoilMoisture(analog);
   Serial.println("Raw: " + String(analog)  + " Percent: " + String(percent));
-  Blynk.virtualWrite(V4, percent);
+  Blynk.virtualWrite(V5, percent);
+  Blynk.virtualWrite(V6, percent);
 }
 
 void setup()
