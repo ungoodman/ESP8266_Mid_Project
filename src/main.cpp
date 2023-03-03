b76a7679c159470234775347d91ba852ffbf5842
ungoodman <suchanej.19@gmail.com>
2023-03-03 23:16:18 +0700

    fix brownout

--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,99 @@
 #include <Arduino.h>
-#include <main.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 
-BLYNK_WORK_MODE_BTN
+#define BLYNK_TEMPLATE_ID "TMPLNkJ1LwdT"
+#define BLYNK_TEMPLATE_NAME "Quickstart Device"
+#define BLYNK_AUTH_TOKEN "ZjlFnFMb_n3lW9SnwSZ8tnKuIVeQ243I"
+#define BLYNK_PRINT Serial
+
+
+char ssid[] = "xxxxx";
+char pass[] = "xxxxx";
+
+bool flagDryLimit = false;
+
+#define ACTIVE_LOW
+#ifdef ACTIVE_LOW
+bool solenoidStatus1 = HIGH;
+bool solenoidStatus2 = HIGH;
+#else
+bool solenoidStatus1 = LOW;
+bool solenoidStatus2 = LOW;
+#endif
+
+BlynkTimer timer;
+
+WidgetLED sor1(V5);
+WidgetLED sor2(V6);
+
+BLYNK_WRITE(V0)
 {
-  isAutomatic = !param.asInt();
+  int value = param.asInt();
+
+  Blynk.virtualWrite(V1, value);
 }
 
-BLYNK_SOLENOID_ZONE_A_BTN
+BLYNK_CONNECTED()
 {
-  if (param.asInt())
-  {
-    virtualSolenoidLedZoneA.on();
-    solenoidZoneA.on();
-  }
-  else
-  {
-    virtualSolenoidLedZoneA.off();
-    solenoidZoneA.off();
-  }
+  Blynk.setProperty(V3, "offImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
+  Blynk.setProperty(V3, "onImageUrl", "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
+  Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
 }
 
-BLYNK_SOLENOID_ZONE_B_BTN
+void checkSoilMoisture(int rawMoistureValue)
 {
-  if (param.asInt())
+  if (rawMoistureValue > 700 && flagDryLimit == false)
   {
-    virtualSolenoidLedZoneB.on();
-    solenoidZoneB.on();
-  }
-  else
-  {
-    virtualSolenoidLedZoneB.off();
-    solenoidZoneB.off();
-  }
-}
+    solenoidStatus1 = LOW;
+    solenoidStatus2 = LOW;
 
-void automaticMode()
-{
-  if (moistureSensor.dryLimitReach())
-  {
-    virtualSolenoidLedZoneA.on();
-    solenoidZoneA.on();
-    virtualSolenoidLedZoneB.on();
-    solenoidZoneB.on();
+    digitalWrite(16, solenoidStatus1);
+    digitalWrite(5, solenoidStatus2);
+
+    sor1.on();
+    sor2.on();
+
+    flagDryLimit = true;
     return;
   }
 
-  if (moistureSensor.wetLimitReach())
+  if (rawMoistureValue < 400 && flagDryLimit == true)
   {
-    virtualSolenoidLedZoneA.off();
-    solenoidZoneA.off();
-    virtualSolenoidLedZoneB.off();
-    solenoidZoneB.off();
+    solenoidStatus1 = HIGH;
+    solenoidStatus2 = HIGH;
+
+    digitalWrite(16, solenoidStatus1);
+    digitalWrite(5, solenoidStatus2);
+
+    sor1.off();
+    sor2.off();
+
+    flagDryLimit = false;
+    return;
   }
 }
 
-void mainEvent()
+void myTimerEvent()
 {
-  float moisturePercent = moistureSensor.getPercent();
-  Blynk.virtualWrite(VIRTUAL_MOISTURE_GAUGE, moisturePercent);
-
-  if (isAutomatic)
-    automaticMode();
+  int analog = analogRead(A0);
+  float percent = (102 - (analog / 10));
+  checkSoilMoisture(analog);
+  Serial.println("Raw: " + String(analog) + " Percent: " + String(percent));
+  Blynk.virtualWrite(V4, percent);
 }
 
 void setup()
 {
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  timer.setInterval(BLYNK_TIMER_INTERVAL, mainEvent);
+  pinMode(A0, OUTPUT);
+  pinMode(16, INPUT);
+  pinMode(5, INPUT);
+  // Debug console
+  Serial.begin(115200);
 
-  Serial.begin(SERIAL_MONITOR_SPEED);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  timer.setInterval(1000L, myTimerEvent);
 }
 
 void loop()
