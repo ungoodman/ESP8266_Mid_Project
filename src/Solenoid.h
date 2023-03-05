4c6a2d731201b14f03ffd2a1342263de05903495
ungoodman <suchanej.19@gmail.com>
2023-03-05 23:27:47 +0700

    Solenoid fix bug

--- src/Solenoid.h
+++ src/Solenoid.h
@@ -5,29 +5,22 @@ class Solenoid
 private:
     // Variables
     int _solenoidPin;
-    int _ledPin;
     bool _activeMode;
     
     // Functions
-    void setup();
+    
 
 public:
-    Solenoid(int solenoidPin, int ledPin);
-    Solenoid(int solenoidPin, int ledPin, bool activeMode);
+    Solenoid(int solenoidPin, bool activeMode);
+    void setup();
     // ~Solenoid();
     void on();
     void off();
 };
 
-Solenoid::Solenoid(int solenoidPin, int ledPin)
-{
-    Solenoid(solenoidPin, ledPin, HIGH);
-}
-
-Solenoid::Solenoid(int solenoidPin, int ledPin, bool activeMode)
+Solenoid::Solenoid(int solenoidPin, bool activeMode = HIGH)
 {
     _solenoidPin = solenoidPin;
-    _ledPin = ledPin;
     _activeMode = activeMode;
 
     setup();
@@ -40,13 +33,12 @@ Solenoid::Solenoid(int solenoidPin, int ledPin, bool activeMode)
 void Solenoid::setup()
 {
     pinMode(_solenoidPin, OUTPUT);
-    pinMode(_ledPin, OUTPUT);
 
     digitalWrite(_solenoidPin, !_activeMode);
 }
 
 void Solenoid::on()
-{
+{   
     digitalWrite(_solenoidPin, _activeMode);
 }
 
